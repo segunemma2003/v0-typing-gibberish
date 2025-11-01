3602d6d7043d42b18958ae8899ad4d3c6d26e4fd
yongpresido <youngpresido94@gmail.com>
2025-11-01 16:48:52 -0400

    a

--- next.config.mjs
+++ next.config.mjs
@@ -13,24 +13,6 @@ const nextConfig = {
   trailingSlash: false,
   // Uncomment below for standalone output (better for VPS, but requires PM2 script update)
   // output: 'standalone',
-  // Explicitly configure CSS processing to use cssnano to bypass lightningcss issues
-  webpack: (config, { isServer, defaultLoaders }) => {
-    // Only apply this customization for CSS minification during the build process
-    if (!isServer && config.optimization && config.optimization.minimizer) {
-      const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-      config.optimization.minimizer = config.optimization.minimizer.map((minimizer) => {
-        if (minimizer.constructor.name === 'CssMinimizerPlugin') {
-          return new CssMinimizerPlugin({
-            minimizerOptions: {
-              preset: 'default',
-            },
-          });
-        }
-        return minimizer;
-      });
-    }
-    return config;
-  },
 }
 
 export default nextConfig
